3904549bce16dec1120e38436707e07ea50dba57
Max <maxtiefengraber@proton.me>
2024-02-19 22:53:28 +0100

    feat: added BlogPostService for Business logic managing BlogPosts and interactions with Database

---
+++ src/main/kotlin/com/max/website/service/BlogPostService.kt
@@ -0,0 +1,41 @@
+package com.max.website.service
+
+import com.max.website.dto.BlogPostDto
+import com.max.website.model.BlogPost
+import com.max.website.repository.BlogPostRepository
+import org.springframework.stereotype.Service
+
+@Service
+class BlogPostService(private val blogPostRepository: BlogPostRepository) {
+
+    fun findAllPosts(): List<BlogPost> = blogPostRepository.findAll()
+
+    fun savePost(post: BlogPost): BlogPost = blogPostRepository.save(post)
+
+    fun findPostById(id: Long): BlogPost? = blogPostRepository.findById(id).orElse(null)
+
+    fun deletePostById(id: Long) = blogPostRepository.deleteById(id)
+
+    fun convertToEntity(dto: BlogPostDto): BlogPost {
+        return BlogPost(
+            title = dto.title,
+            content = dto.content,
+            author = dto.author,
+            // Only set ID and createdAt if non-null, otherwise let the database handle it
+            id = dto.id ?: 0,
+            createdAt = dto.createdAt
+        )
+    }
+
+    fun convertToDto(entity: BlogPost): BlogPostDto {
+        return BlogPostDto(
+            id = entity.id,
+            title = entity.title,
+            content = entity.content,
+            author = entity.author,
+            createdAt = entity.createdAt
+        )
+    }
+
+
+}
