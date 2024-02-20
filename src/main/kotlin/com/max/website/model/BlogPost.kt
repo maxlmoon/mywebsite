979faf44bb5b1e9350c9cb037a1f898db4be4c51
Max <maxtiefengraber@proton.me>
2024-02-20 23:50:39 +0100

    Revert "feat: add BlogPost.kt entity, mapping to a database table"
    This reverts commit d03594b7805422b2929e64bf17291604e1a13a17.

--- src/main/kotlin/com/max/website/model/BlogPost.kt
+++ src/main/kotlin/com/max/website/model/BlogPost.kt
@@ -1,25 +1,25 @@
 package com.max.website.model
 
-import jakarta.persistence.*
-import jakarta.persistence.Id
 import java.time.LocalDateTime
+import javax.persistence.*
 
 @Entity
 @Table(name = "blog_posts")
 data class BlogPost(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0,
 
-    @Column(nullable = false)
-    var title: String,
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long = 0,
 
-    @Column(nullable = false, length = 10000)
-    var content: String,
+        @Column(nullable = false)
+        var title: String,
 
-    @Column(nullable = false)
-    var author: String,
+        @Column(nullable = false, length = 10000)
+        var content: String,
 
-    @Column(nullable = false)
-    var createdAt: LocalDateTime = LocalDateTime.now()
+        @Column(nullable = false)
+        var author: String,
+
+        @Column(nullable = false)
+        var createdAt: LocalDateTime = LocalDateTime.now()
 )
